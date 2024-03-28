3d440e4dfee5094e8c8015073f1fd7b3846547ce
Hakan <95884021+hakancyln@users.noreply.github.com>
2024-03-28 16:10:56 +0300

    Delete sil directory

--- sil/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sil.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
